4f2d1ad3858c42a0b95d3a1b64a633118de015c1
Pavel Nyukalo <PashaOktan07@gmail.com>
2020-02-19 23:32:17 +0500

    Исправления для тревиса

--- js/simular.js
+++ js/simular.js
@@ -18,16 +18,6 @@
     return rank;
   };
 
-  var namesComparator = function (left, right) {
-    if (left > right) {
-      return 1;
-    } else if (left < right) {
-      return -1;
-    } else {
-      return 0;
-    }
-  };
-
   var updateWizards = function () {
     window.wizard.render(wizards.slice().
       sort(function (left, right) {
